77f68cd57131b9e9e2c12b0b16b2adb87c2c4686
yosshii8888 <138771166+yosshii8888@users.noreply.github.com>
2025-02-12 15:55:41 +0900

    一旦
    まだ動かない

--- testcode/GPS/GPS/main.cpp
+++ testcode/GPS/GPS/main.cpp
@@ -9,20 +9,22 @@
 #include "hardware/uart.h"
 
 
-#define UART_ID uart1
-#define BAUD_RATE 39200  // ビットレートこれだっけ？
+
+#define UART_ID uart0
+#define BAUD_RATE 38400  // ビットレートこれだっけ？
 #define DATA_BITS 8
 #define STOP_BITS 1
 #define PARITY    UART_PARITY_NONE
 
-#define UART_TX_PIN 4
-#define UART_RX_PIN 5
+#define UART_TX_PIN 0
+#define UART_RX_PIN 1
 
-static int chars_rxed = 0;
+// static int chars_rxed = 0;
 
 char uart_buffer[256];
 
 // RX interrupt handler
+/*  割り込み無効化
 void on_uart_rx() {
     while (uart_is_readable(UART_ID)) {
         // uint8_t ch = uart_getc(UART_ID);
@@ -36,16 +38,37 @@ void on_uart_rx() {
         chars_rxed++;
     }
 }
+*/
 
 int Serial_receive(char *buffer, size_t length) {
     size_t count = 0;
+    
+    // タイムアウト
+    auto start_time = get_absolute_time();
+    const int TIMEOUT_MS = 10*1000000;  // マイクロ秒
+
+    // テスト用
+    std::cout << length << std::endl;
+    std::cout << "uart_is_readable: " << uart_is_readable(UART_ID) << std::endl;
+
 
     while (count < length) {
         // データが受信可能か確認
         if (uart_is_readable(UART_ID)) {
             // 受信したデータをバッファに格納
             buffer[count++] = uart_getc(UART_ID);
+            // テスト用
+            std::cout << count << std::endl;
         }
+        
+        // タイムアウト
+        auto now = get_absolute_time();
+        if ((now - start_time) > TIMEOUT_MS) {
+            std::cout << "Timeout reached" << std::endl;
+            break;
+        }
+
+        // sleep_ms(10);
     }
     return count; // 受信したバイト数を返す
 }
@@ -90,33 +113,44 @@ int main() {
   // Set up a RX interrupt
   // We need to set up the handler first
   // Select correct interrupt for the UART we are using
-  int UART_IRQ = UART_ID == uart0 ? UART0_IRQ : UART1_IRQ;
-  
-  // And set up and enable the interrupt handlers
-  irq_set_exclusive_handler(UART_IRQ, on_uart_rx);
-  irq_set_enabled(UART_IRQ, true);
-  
-  // Now enable the UART to send interrupts - RX only
-  uart_set_irq_enables(UART_ID, true, false);
+
+// 割り込み無効化
+//   int UART_IRQ = UART_ID == uart0 ? UART0_IRQ : UART1_IRQ;
+//   irq_set_exclusive_handler(UART_IRQ, on_uart_rx);
+//   irq_set_enabled(UART_IRQ, true);
+//   uart_set_irq_enables(UART_ID, true, false);
 
 
   NEMAParser::NEMAParser parser;
   NEMAParser::ErrorCode error;
+
+  gpio_init(PICO_DEFAULT_LED_PIN);
+  gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+  gpio_put(PICO_DEFAULT_LED_PIN, 0);
+  gpio_put(PICO_DEFAULT_LED_PIN, 1);
+  sleep_ms(250);
+
   while (true) {
+    std::cout << "hello, gps" << std::endl;
+
     sleep_us(1000000.f); // 100ms
     ofs_all_log << "-----START-----" << std::endl;
+    std::cout << "-----START-----" << std::endl;
     // std::istringstream ss(uart_getc(UART_ID, '\n'y));
     
     int received_bytes = Serial_receive(uart_buffer, sizeof(uart_buffer) - 1);
-    std::istringstream ss(uart_buffer);
+    std::cout << received_bytes << std::endl;
     
-    std::string buffer;
-    while (std::getline(ss, buffer, '\n') ) {
-     error = parser.parse(buffer);
+    std::istringstream ss(uart_buffer);
+    std::string line;
+    while (std::getline(ss, line, '\n')) {
+        error = parser.parse(line);
+        if (error != NEMAParser::ErrorCode::kNoError) {
+            ofs_all_log << "Error Code = " << static_cast<int>(error) << std::endl;
+            std::cout << "Error Code = " << static_cast<int>(error) << std::endl;
+        }
     }
-    if (error != NEMAParser::ErrorCode::kNoError) {
-      ofs_all_log << "Error Code = " << (int)error << std::endl;
-    }
+
     parser.getGPGGA().output(ofs_all_log);
     parser.getGPGLL().output(ofs_all_log);
     parser.getGPGSA().output(ofs_all_log);
@@ -125,6 +159,7 @@ int main() {
     parser.getGPVTG().output(ofs_all_log);
     parser.getGPZDA().output(ofs_all_log);
     ofs_all_log << "-----END-----" << std::endl;
+    std::cout << "-----END-----" << std::endl;
 
     ofs_some_log
         << std::setprecision(10)
@@ -140,6 +175,22 @@ int main() {
         << parser.getGPGSA().status << ","
         << parser.getGPGGA().number_of_satellites
         << std::endl;
+    
+    std::cout
+        << std::setprecision(10)
+        << parser.getGPGGA().time.utc << ","
+        << parser.getGPGGA().latitude.decimal_degrees << ","
+        << parser.getGPGGA().latitude_direction << ","
+        << parser.getGPGGA().longitude.decimal_degrees << ","
+        << parser.getGPGGA().longitude_direction << ","
+        << parser.getGPGGA().altitude << ","
+        << parser.getGPVTG().ground_speed_kmh << ","
+        << parser.getGPVTG().track << ","
+        << parser.getGPGGA().quality << ","
+        << parser.getGPGSA().status << ","
+        << parser.getGPGGA().number_of_satellites
+        << std::endl;
+    
   }
   return 0;
 }
